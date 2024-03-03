dafab771079e7555c8586001ea55ca109399120a
MartaKoss27 <27691497m@gmail.com>
2024-03-03 14:34:20 +0100

    400 fix 2

--- src/hook/useAddUser.js
+++ src/hook/useAddUser.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+//import axios from 'axios';
 
-export const useAddUser = async user => {
-  try {
-    await axios.post('', user);
-  } catch (error) {
-    return console.log(error);
-  }
-};
+//export const useAddUser = async user => {
+//try {
+//await axios.post('https://65d79be327d9a3bc1d7b7bb8.mockapi.io', user);
+//} catch (error) {
+// return console.log(error);
+//}
+//};
