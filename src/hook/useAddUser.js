fef319aa602ce778f86aec2bb8adb8690330327f
MartaKoss27 <27691497m@gmail.com>
2024-03-03 14:23:40 +0100

    400 post fix

--- src/hook/useAddUser.js
+++ src/hook/useAddUser.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const useAddUser = async user => {
   try {
-    await axios.post('https://656008da83aba11d99d01e5e.mockapi.io/users', user);
+    await axios.post('', user);
   } catch (error) {
     return console.log(error);
   }
